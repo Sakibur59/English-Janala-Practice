0b6fd99e60f18e1cc98c1c90db1c79c623555354
Sakibur59 <sakiburrahman5978@gmail.com>
2026-03-03 12:30:24 +0600

    word display section are added

--- script/index.js
+++ script/index.js
@@ -4,6 +4,36 @@ const loadData = () => {
     .then((json) => displayLesson(json.data));
 };
 
+const loadWord = (id) =>{
+  const url = `https://openapi.programming-hero.com/api/level/${id}`
+  fetch(url)
+  .then(res => res.json())
+  .then((data)=>displayWord(data.data))
+  
+}
+
+const displayWord = (words) => {
+  const wordContainer = document.getElementById("word-container");
+  wordContainer.innerHTML=""
+
+   words.forEach((word) => {
+    const wordDiv = document.createElement("div");
+    wordDiv.innerHTML = `
+        <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
+          <h2 class="font-bold text-2xl">${word.word}</h2>
+          <p class="font-semibold">Meaning / Pronunciation</p>
+          <div class="text-2xl font-medium font-bangla">${word.meaning} / ${word.pronunciation}</div>
+          <div class="flex justify-between items-center">
+            <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
+            <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
+          </div>
+        </div>
+        `;
+
+    wordContainer.append(wordDiv);
+   })
+};
+
 const displayLesson = (lesson) => {
   const lessonContainer = document.getElementById("lesson-container");
   lessonContainer.innerHTML = "";
@@ -11,7 +41,7 @@ const displayLesson = (lesson) => {
   for (let element of lesson) {
     const lessonDiv = document.createElement("div");
     lessonDiv.innerHTML = `
-        <button  class="btn btn-outline btn-primary"
+        <button onclick="loadWord(${element.level_no})"  class="btn btn-outline btn-primary"
                 ><i class="fa-solid fa-book-open"></i>Lesson- ${element.level_no}</button
         `;
 
